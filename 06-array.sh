c41fca81863f16bf5550de5989657aec4030f0e3
ani <anitha.s.goutham@gmail.com>
2024-08-27 22:53:09 +0800

    command

--- 06-array.sh
+++ 06-array.sh
@@ -6,4 +6,4 @@ echo "first fruit is : ${fruits[0]}"
 echo "first fruit is : ${fruits[1]}"
 echo "first fruit is : ${fruits[2]}"
 
-echo "first fruit is : ${fruits[@ ]}"
+echo "first fruit is : ${fruits[@]}"
