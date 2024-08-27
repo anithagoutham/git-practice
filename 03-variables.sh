4479ab3574551be1e59fd7614474af5c8b26c385
ani <anitha.s.goutham@gmail.com>
2024-08-27 16:04:10 +0800

    variables program

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+$person1=rameshperson2=suresh
+
+echo "$person1:hi $person2"
+echo "$person2:hi $person1 i am fine how are u"
+echo "$person1:i am good what up man"
+echo "$person2:i started learning shellscripting"
