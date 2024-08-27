18b4844077a28fae8ce5ecfe4648dd912b280194
ani <anitha.s.goutham@gmail.com>
2024-08-27 23:57:00 +0800

    date format

---
+++ 09-special-var.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo " all variables passed to the script: $@"
+echo " how many variables passed to the script: $#"
+echo " how to check our script name: $0"
+echo " to check current working directory: $PWD"
+echo " to check home directory: $HOME"
+echo " to check pid: $$"
+sleep 100 &
+
+echo " pid of last background command: $!"
