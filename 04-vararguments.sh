013edabb7a32d203c79f1c45cf4551278b1f12d6
ani <anitha.s.goutham@gmail.com>
2024-08-27 16:14:19 +0800

    vararguments

---
+++ 04-vararguments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1:hi $person2"
+echo "$person2:hi $person1 i am fine how are u"
+echo "$person1:i am good what up man"
+echo "$person2:i started learning shellscripting"
