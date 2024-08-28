3c6eec17429664378cc48bb0d4900e998695089b
ani <anitha.s.goutham@gmail.com>
2024-08-28 13:37:48 +0800

    my shell programs

--- 09-special-var.sh
+++ 09-special-var.sh
@@ -4,8 +4,7 @@ echo " all variables passed to the script: $@"
 echo " how many variables passed to the script: $#"
 echo " how to check our script name: $0"
 echo " to check current working directory: $PWD"
-echo " to check home directory: $home"
+echo " to check home directory: $HOME"
 echo " to check pid: $$"
 sleep 100 &
-
 echo " pid of last background command: $!"
