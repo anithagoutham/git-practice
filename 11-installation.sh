eebad23d2092f9ec4daa4974111626250055024f
ani <anitha.s.goutham@gmail.com>
2024-08-28 15:23:52 +0800

    my shell programs

--- 11-installation.sh
+++ 11-installation.sh
@@ -11,4 +11,14 @@ then
 
 fi
 
-dnf install git -y
+dnf list install git -y
+
+if [ $? -ne 0 ]
+then 
+  echo " git is not installed, then install it "
+   dnf install git -y
+
+else
+  echo " git is already installed "
+
+fi   
